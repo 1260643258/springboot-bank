b61a6924063de569cf1db161a3a2e1d068ae095c
zhengshuohong <zhengshuohong@163.com>
2019-07-03 20:53:53 +0800

    将springboot项目加入服务注册

--- bank/src/main/java/com/example/bank/BankApplication.java
+++ bank/src/main/java/com/example/bank/BankApplication.java
@@ -2,9 +2,12 @@ package com.example.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableSwagger2
 public class BankApplication {
